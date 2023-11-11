5c1a35b0c74e9e268421604275c5047e1662d1cc
Justin <justinanthony.primary@gmail.com>
2023-11-11 22:51:20 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id='test'>
+    <h1>Multi-form tests</h1>
+    <div v-for='(loopData, index) in loopTest' :class='{ hidden: true, active: formPosition === index + 1 }' :key='index'>
+      {{ loopData.name }}
+      <input type='text' :placeholder='loopData.placeholder' />
+    </div>
+    <div>
+      <button @click='previous'>Previous</button>
+      <button @click='next'>Next</button>
+      <button>Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        formPosition: 1,
+        loopTest: [{
+          name: 'Title 1',
+          placeholder: 'Name'
+        }, {
+          name: 'Title 2',
+          placeholder: 'Age'
+        }, {
+          name: 'Title 3',
+          placeholder: 'Address'
+        }]
+      }
+    },
+    methods: {
+      next() {
+        if (this.formPosition < this.loopTest.length) {
+          this.formPosition += 1
+        }
+      },
+      previous() {
+        if (this.formPosition > 1) {
+          this.formPosition -= 1
+        }
+      }
+    }
+  }
+</script>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Test from '../components/Test.vue'
+</script>
+
+<template>
+  <main>
+    <Test />
+  </main>
+</template>
